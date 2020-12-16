005bdca24f3632381b1da87c7617364d5b975b27
ZYY0911 <605061405@qq.com>
2020-12-16 20:27:37 +0800

    第5次提交

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+            setContentView(R.layout.activity_main);
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        }
 }
